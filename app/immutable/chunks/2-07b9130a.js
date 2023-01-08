67db5242d5582741255e5cac38c810448e7caba5
magn2998 <magnus.fjellerup@gmail.com>
2023-01-08 19:06:29 +0100

    New Design

--- app/immutable/chunks/2-07b9130a.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./page-da46b06b.js";import{default as t}from"../components/pages/page.svelte-c9474ece.js";export{t as component,r as shared};
---
+++ app/immutable/chunks/2-40a6a583.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./page-da46b06b.js";import{default as t}from"../components/pages/page.svelte-6c90ead0.js";export{t as component,r as shared};
